d485d0833ec19e85d0888f72212fefcea78b3b85
David Driscoll <david.driscoll@gmail.com>
2020-09-13 22:36:23 -0400

    initial commit

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    devServer: {
+        proxy: {
+            '^/api': {
+                target: 'http://localhost:7071',
+                ws: true,
+                changeOrigin: true,
+            },
+        },
+    },
+};
