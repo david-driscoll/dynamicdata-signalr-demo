827ed610d07351de973a26430dfc20c241214576
David Driscoll <david.driscoll@gmail.com>
2020-09-13 23:55:55 -0400

    dump config

--- api/dumpConfig.cs
+++ api/dumpConfig.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System.Text;
 using System.Collections;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+using Microsoft.AspNetCore.SignalR;
 
 namespace Company.Function
 {
@@ -25,6 +27,34 @@ namespace Company.Function
                 sb.AppendLine($"{item.Key}: {item.Value}");
             }
             return new OkObjectResult(sb.ToString());
+        }
+    }
+
+    public class HerosHub : ServerlessHub
+    {
+        [FunctionName("negotiate")]
+        public SignalRConnectionInfo Negotiate([HttpTrigger(AuthorizationLevel.Anonymous)] HttpRequest req)
+        {
+            return Negotiate(req.Headers["x-ms-signalr-user-id"], GetClaims(req.Headers["Authorization"]));
+        }
+
+        [FunctionName(nameof(OnConnected))]
+        public async Task OnConnected([SignalRTrigger] InvocationContext invocationContext, ILogger logger)
+        {
+            // await Clients.All.SendAsync(NewConnectionTarget, new NewConnection(invocationContext.ConnectionId));
+            logger.LogInformation($"{invocationContext.ConnectionId} has connected");
+        }
+
+        [FunctionName(nameof(Broadcast))]
+        public async Task Broadcast([SignalRTrigger] InvocationContext invocationContext, string message, ILogger logger)
+        {
+            // await Clients.All.SendAsync(NewMessageTarget, new NewMessage(invocationContext, message));
+            logger.LogInformation($"{invocationContext.ConnectionId} broadcast {message}");
+        }
+
+        [FunctionName(nameof(OnDisconnected))]
+        public void OnDisconnected([SignalRTrigger] InvocationContext invocationContext)
+        {
         }
     }
 }
